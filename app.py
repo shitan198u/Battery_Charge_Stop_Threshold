72921744eaa74dd25d6ad4227516f40d80000c55
Shitanshu Shivam <shiva.shitanshu2018@gmail.com>
2023-06-26 01:35:30 +0530

    with all changes as a complete solution

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import subprocess
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QSpinBox, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5.QtCore import Qt
@@ -11,29 +12,73 @@ class BatteryChargeThresholdApp(QWidget):
         self.setWindowTitle("Battery Charge Threshold")
         self.setWindowFlag(Qt.WindowStaysOnTopHint)  # Set the window to stay on top
         self.setFixedSize(300, 150)
-        # self.setGeometry(300, 300, 150, 150)
-        
+
+        # Create the label
         self.label = QLabel("Set Battery Charge Threshold (%):")
+
+        # Create the spin box to set the threshold value
         self.spinBox = QSpinBox()
-        self.spinBox.setRange(20, 100)
-        self.spinBox.setValue(80)  # Default value of 80%
+        self.spinBox.setRange(20, 100)  # Set the range from 20 to 100
+
+        # Get the current threshold value and set it as the default value of the spin box
+        current_threshold = self.get_current_threshold()
+        if current_threshold:
+            self.spinBox.setValue(current_threshold)
+        else:
+            self.spinBox.setValue(80)  # Default value of 80%
+
+        # Create the button to set the threshold
         self.button = QPushButton("Set Threshold")
         self.button.clicked.connect(self.set_threshold)
 
+        # Create the layout and add the widgets
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.spinBox)
         layout.addWidget(self.button)
         self.setLayout(layout)
-        
-    def set_threshold(self):
-        new_threshold = self.spinBox.value()
-        
+
+    def get_current_threshold(self):
         try:
-            subprocess.run(["pkexec", "bash", "-c", "/home/shsrra/CODE/GUI/Battery_Charge/modify_threshold.sh {}".format(new_threshold)], check=True)
-        except subprocess.CalledProcessError:
-            QMessageBox.critical(self, "Error", "Failed to set battery charge threshold.")
+            # Get the name of the battery by running the ls command
+            battery_name = subprocess.check_output(["ls", "/sys/class/power_supply"]).decode().strip().split("\n")[-1]
+
+            # Construct the path to the charge_control_end_threshold file using the battery name
+            threshold_path = f"/sys/class/power_supply/{battery_name}/charge_control_end_threshold"
+
+            # Read the current threshold value from the charge_control_end_threshold file
+            with open(threshold_path, "r") as f:
+                current_threshold = int(f.read().strip())
+                return current_threshold
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+    def set_threshold(self):
+        # Get the new threshold value from the spin box
+        new_threshold = self.spinBox.value()
+
+        try:
+            # Get the present working directory
+            current_directory = os.getcwd()
+            
+            # Construct the path to the shell script using the present working directory
+            script_path = os.path.join(current_directory, "modify_threshold.sh")
+
+            # Check if the shell script exists and is executable
+            if not os.path.isfile(script_path) or not os.access(script_path, os.X_OK):
+                raise Exception("Shell script not found or not executable")
+
+            # Execute the shell script with pkexec and pass the new threshold value as an argument
+            subprocess.run(["pkexec", "bash", "-c", "{} {}".format(script_path, new_threshold)], check=True)
+
+        except Exception as e:
+            # Display an error message if something goes wrong
+            QMessageBox.critical(self, "Error", str(e))
+        
         else:
+            # Display a success message with the new threshold value
             QMessageBox.information(self, "Success", f"Battery charge threshold set to {new_threshold}%.")
 
 
--- modify_threshold.sh
+++ modify_threshold.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
 
-current_threshold=$(cat /sys/class/power_supply/BAT0/charge_control_end_threshold)
+# Check if the script is being run with root privileges
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Check if the input argument is within an acceptable range
+if [ "$1" -lt 20 ] || [ "$1" -gt 100 ]
+  then echo "Invalid input: threshold must be between 20 and 100"
+  exit
+fi
+
+# Get the name of the battery by running the ls command
+battery_name=$(ls /sys/class/power_supply | grep '^BAT')
+
+# Check if the charge_control_end_threshold file exists
+if [ ! -f "/sys/class/power_supply/$battery_name/charge_control_end_threshold" ]; then
+    echo "Error: charge_control_end_threshold file not found"
+    exit 1
+fi
+
+# Check if the battery-charge-threshold.service file exists
+if [ ! -f "/etc/systemd/system/battery-charge-threshold.service" ]; then
+    # Create the battery-charge-threshold.service file
+    cat > /etc/systemd/system/battery-charge-threshold.service << EOF
+[Unit]
+Description=Set the battery charge threshold
+After=multi-user.target
+
+StartLimitBurst=0
+
+[Service]
+Type=oneshot
+Restart=on-failure
+
+ExecStart=/bin/bash -c 'echo 80 > /sys/class/power_supply/$battery_name/charge_control_end_threshold'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    # Enable and start the battery-charge-threshold service
+    systemctl enable battery-charge-threshold.service
+    systemctl start battery-charge-threshold.service
+fi
+
+# Get the current threshold value
+current_threshold=$(cat /sys/class/power_supply/$battery_name/charge_control_end_threshold)
 new_threshold=$1
+
+# Update the battery-charge-threshold.service file with the new threshold value
 sed -i "s/$current_threshold/$new_threshold/g" /etc/systemd/system/battery-charge-threshold.service
-# systemctl stop battery-charge-threshold.service
-# systemctl disable battery-charge-threshold.service
-# systemctl daemon-reload
-# systemctl enable battery-charge-threshold.service
-# systemctl start battery-charge-threshold.service
+
+# Reload the systemd daemon and restart the battery-charge-threshold service
 systemctl daemon-reload
 systemctl restart battery-charge-threshold.service
